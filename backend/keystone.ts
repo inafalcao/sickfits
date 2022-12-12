20b63c3372c3eb34f2b3f89f34c80cecc10b7257
Ina <inafalcao@gmail.com>
2022-12-12 17:15:06 -0300

    Seeding data.

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -8,6 +8,7 @@ import {
 import { User } from './schemas/User';
 import { Product } from './schemas/Product';
 import { ProductImage } from './schemas/ProductImage';
+import { insertSeedData } from './seed-data';
 
 const databaseURL = process.env.DATABASE_URL || 'local-db-url';
 
@@ -37,6 +38,12 @@ export default withAuth(
     db: {
       adapter: 'mongoose',
       url: databaseURL,
+      async onConnect(keystone) {
+        console.log(keystone);
+        if (process.argv.includes('--seed-data')) {
+          await insertSeedData(keystone);
+        }
+      },
     },
     lists: createSchema({ User, Product, ProductImage }),
     ui: {
