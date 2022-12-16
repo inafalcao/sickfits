ae076242d6bd898cbac2f704a07f4cc607d1a7bb
Ina <inafalcao@gmail.com>
2022-12-16 18:29:52 -0300

    Implementing search.

--- backend/mutations/addToCart.ts
+++ backend/mutations/addToCart.ts
@@ -8,34 +8,18 @@ async function addToCart(
   { productId }: { productId: string },
   context: KeystoneContext
 ): Promise<CartItemsCreateInput> {
-  console.log('adding to cart!!!');
   const session = context.session as Session;
 
   if (!session.itemId) {
     throw new Error('You must be logged in to do this.');
   }
 
-  console.log("product id");
-  console.log(productId);
-
-
-  console.log("SESSION ID")
-  console.log(session.itemId);
-
-
   const allCartItems = await context.lists.CartItem.findMany({
     where: { user: { id: session.itemId }, product: { id: productId } },
     resolveFields: 'id,quantity'
   });
 
-  console.log("ALL ITEMS");
-
-  console.log(allCartItems)
-
   const [existingCartItem] = allCartItems;
-  console.log("EXISTING ITEM");
-
-  console.log(existingCartItem)
   if (existingCartItem) {
     return await context.lists.CartItem.updateOne({
       id: existingCartItem.id,
